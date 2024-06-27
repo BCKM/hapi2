9b6b1259ecd39eeba9534f34faa9b476ac854e41
Komron <knmv2311@gmail.com>
2024-06-27 12:04:29 +0500

    bu

--- dbconfig/index.js
+++ dbconfig/index.js
@@ -6,8 +6,25 @@ const sequelize = new Sequelize('hapi_tutorial', 'root', 'Km1114_1205aJ', {
     dialect: 'mysql'
 });
 
-sequelize.authenticate().then(() => {
-    console
-}).catch(() => {
 
-})
+module.exports.connect = sequelize
+// module.exports.getUsers = async function testConnection() {
+        
+//         try{
+//             await sequelize.authenticate();
+//             console.log("Connected!")
+//             const [results, metadata] = await sequelize.query('SELECT * FROM users');
+//             return results;
+//         } catch(err) {
+//             console.log("Can't connect to DB!");
+//         }
+
+//     }
+
+
+
+// sequelize.authenticate().then(() => {    // to check if it will connect to db
+//     console.log("Connected to DB!");
+// }).catch(() => {
+//     console.log("Could not connect");
+// })
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 'use strict'; // to avoid creating global variables
 
 const Hapi = require('@hapi/hapi'); // importing
+const { channel } = require('diagnostics_channel');
 const { request } = require('http');
 const path = require('path');
+const Connection = require('./dbconfig');
+const Users = require('./models/users');
 
 const init = async () => {
 
@@ -53,16 +56,16 @@ const init = async () => {
             //     }
             // }
         },
-        {
-            method: 'GET',
-            path: '/dynamic',
-            handler: (request, h) => {
-                const data = {
-                    name: 'Komron'
-                }
-                return h.view('index', data)
-            }
-        },
+        // {
+        //     method: 'GET',
+        //     path: '/dynamic',
+        //     handler: (request, h) => {
+        //         const data = {
+        //             name: 'Komron'
+        //         }
+        //         return h.view('index', data)
+        //     }
+        // },
         {
             method: 'POST',
             path: '/login',
@@ -76,8 +79,8 @@ const init = async () => {
                 // } else {
                 //     return h.redirect('/');
                 // }
-
-                return h.view('index', {username: request.payload.username}); 
+                Users.createUser(request.payload.username, request.payload.password);                
+                return h.view('index', {username: request.payload.username});
             }
         },
         {
@@ -90,46 +93,54 @@ const init = async () => {
                 });
             }
         },
-        {
-            method: 'GET',
-            path: '/location',
-            handler: (request, h) => {
-                if (request.location) {
-                    return h.view('location', {location: request.location.ip});
-                } else {
-                    return h.view('location', {location: "Your location isn't enabled by default"})
-                }
+        // {
+        //     method: 'GET',
+        //     path: '/location',
+        //     handler: (request, h) => {
+        //         if (request.location) {
+        //             return h.view('location', {location: request.location.ip});
+        //         } else {
+        //             return h.view('location', {location: "Your location isn't enabled by default"})
+        //         }
 
-            }
-        },
-        {
-            method: 'GET',
-            path: '/users', // for commented codes below path: '/users/{user?}',
-            handler: (request, h) => {
-                // if (request.params.user){
-                //     return `<h1>Hello ${request.params.user}</h1>`;
-                // } else {
-                //     return 'Hi smbd';
-                // }
+        //     }
+        // },
+        // {
+        //     method: 'GET',
+        //     path: '/users', // for commented codes below path: '/users/{user?}',
+        //     handler: (request, h) => {
+        //         // if (request.params.user){
+        //         //     return `<h1>Hello ${request.params.user}</h1>`;
+        //         // } else {
+        //         //     return 'Hi smbd';
+        //         // }
 
 
-                // return `<h1>${request.query.name} ${request.query.lastname}</h1>`
+        //         // return `<h1>${request.query.name} ${request.query.lastname}</h1>`
                 
                 
                 
-                // console.log(h);                  // to redirect to main path
-                // return h.redirect('/');
+        //         // console.log(h);                  // to redirect to main path
+        //         // return h.redirect('/');
 
 
-                return "<h1>USERS PAGE</h1>"
+        //         return "<h1>USERS PAGE</h1>"
 
-            }
-        },
+        //     }
+        // },
         {
             method: 'GET',
             path: '/{any*}',
             handler: (request, h) => {
                 return '<h1>you must be lost</h1>'
+            }
+        },
+        {
+            method: 'GET',
+            path: '/getUsers',
+            handler: async (request, h) => {
+                const dbConnection = await Connection.connect;                
+                return h.view('index', {users});
             }
         }
     ]);
